db7512688c6286f16338a40b90eb0c5af2d8be74
Billy Zhao <billyzhaoyh@gmail.com>
2019-03-10 12:24:50 +0000

    last one

--- webpage/display_drug.py
+++ webpage/display_drug.py
@@ -7,18 +7,16 @@ def display_drug_file (api_key,file_id_list):
 	url_base='https://api.todaqfinance.net/files/'
 	drug_history=[]
 	for i in range(len(file_id_list)):
-		url = url_base+file_id
+		url = url_base+file_id_list[i]
 		headers = {
 		  'Content-Type': 'application/json',
 		  'x-api-key': api_key
 		}
 		response = requests.request('GET', url, headers = headers, data = payload, allow_redirects=False)
 		a=response.json()
-		number_of_entries=(len(a['data']))
 		total_record=[]
-		for i in range(number_of_entries):
-			data_field=a['data'][i]
-			record_dict=data_field['attributes']['payload']['assignee-type']
-			total_record.append(record_dict)
+		data_field=a['data']
+		record_dict=data_field['attributes']['payload']['assignee_type']
+		total_record.append(record_dict)
 		drug_history.append(total_record)
 	return drug_history

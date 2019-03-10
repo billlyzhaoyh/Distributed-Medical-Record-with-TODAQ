7b2a84657c8cfc1db84186757faa17d81dc768d1
Billy Zhao <billyzhaoyh@gmail.com>
2019-03-10 11:20:22 +0000

    delete

--- data_from_form.py
+++
@@ -1,4 +0,0 @@
-import json
-payload= '{\n\t\"data\": {\n    \t\"relationships\":{\n    \t\t\"sender\":{\n    \t\t\t\"data\": {\n\t    \t\t\t\"type\":\"account\",\n    \t\t\t\t\"id\":\"28cd9275-fcdc-4611-9203-7c64621b9efe\"\n    \t\t\t}\n    \t\t},\n    \t\t\"recipient\":{\n    \t\t\t\"data\": {\n\t    \t\t\t\"type\":\"account\",\n    \t\t\t\t\"id\":\"0c39911e-f73a-45d7-b120-b5929a5f3385\"\n    \t\t\t}\n    \t\t},\n    \t\t\"files\":{\n    \t\t\t\"data\":[\n    \t\t\t\t{\n    \t\t\t\t\t\"type\":\"file\",\n\t\t    \t\t\t\"id\":\"7426a353a1908c70d049797ce93ee97821b94233aef9b758ef9600e9aaa520c5\"\n    \t\t\t\t}\n\t\t\t\t]\n    \t\t}\n    \t}\n    }\n}'
-python_payload = json.load(payload)
-print(python_payload)
